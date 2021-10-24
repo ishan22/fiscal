53a79a1bb60e7d33c50b373122a01b3d1f6f46aa
Ishan Mitra <imitra@LAPTOP-NTD2TN0S.localdomain>
2021-10-24 03:40:12 -0700

    pls fix me

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
 var submits = document.querySelectorAll("#submitOrderButtonId, #bottomSubmitOrderButtonId");
 
-var buttonHtml = '<span id="submitFakeOrderButtonId" class="a-button a-button-span12 a-button-primary continue-button celwidget buy-button-height buy-button-sky-fix"><span class="a-button-inner"><input name="fakeOrderPlace" id="fakeOrder" class="a-button-input" type="button"><span id="submitFakeOrderButtonId-announce" class="a-button-text" aria-hidden="true"><span class="place-order-button-text buy-button-line-height">Place your order please.</span></span></span></span>';
+var confirmation = '<body><span class="check1"><i class="fa fa-check"></i></span> <span class="font-weight-bold">Order Confirmed</span> <small class="mt-2" >Thank you for your business!</small > <a href="#" class="text-decoration-none invoice-link" >View Invoice</a > <button class="btn btn-danger btn-block order-button"> Return to the homepage </button></body>';
+var webpage = '<!DOCTYPE html><html> <body><h1>bradipshit</h1></body></html>';
+
+var buttonHtml = `<span id="submitFakeOrderButtonId" class="a-button a-button-span12 a-button-primary continue-button celwidget buy-button-height buy-button-sky-fix">
+    <span class="a-button-inner">
+    <input name="fakeOrderPlace" id="fakeOrder" class="a-button-input" type="button" onClick="function() {document.body.innerHTML = ${confirmation}}">
+    <span id="submitFakeOrderButtonId-announce" class="a-button-text" aria-hidden="true">
+    <span class="place-order-button-text buy-button-line-height">Place your order please.</span>
+    </span></span></span>`;
 
 for (let index = 0; index < submits.length; index++) {
     console.log(submits[index].id);
